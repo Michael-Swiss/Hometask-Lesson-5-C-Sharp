505afdf660bf30b502c3b9ef2a8844542e347168
Michael <michael_mosienko@mail.ru>
2023-05-12 17:06:14 +0200

    Added Decision of Homework 5

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿// Console.WriteLine("Введите размер массива");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// Console.WriteLine("Enter array size");
 // int size = Convert.ToInt32(Console.ReadLine());
 // int[] numbers = new int[size];
 // FillArrayRandomNumbers(numbers);
-// Console.WriteLine("Вот наш массив: ");
+// Console.WriteLine("Here is our array: ");
 // PrintArray(numbers);
 // int count = 0;
 
@@ -10,7 +13,7 @@
 // if (numbers[z] % 2 == 0)
 // count++;
 
-// Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+// Console.WriteLine($"Total are {numbers.Length} numbers, {count} of them - are even");
 
 // void FillArrayRandomNumbers(int[] numbers)
 // {
@@ -33,20 +36,22 @@
 
 
 
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
-
-// Console.WriteLine("Введите размер массива");
+// Console.WriteLine("Enter array size");
 // int size = Convert.ToInt32(Console.ReadLine());
 // int[] numbers = new int[size];
 // FillArrayRandomNumbers(numbers);
-// Console.WriteLine("Вот наш массив: ");
+// Console.WriteLine("Here is our array: ");
 // PrintArray(numbers);
 // int sum = 0;
 
 // for (int z = 0; z < numbers.Length; z+=2)
 //     sum = sum + numbers[z];
 
-//     Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+//     Console.WriteLine($"Total are {numbers.Length} numbers, summ of elements situated on odd positions is = {sum}");
 
 // void FillArrayRandomNumbers(int[] numbers)
 // {
@@ -69,12 +74,14 @@
 
 
 
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3,21 7,04 22,93 -2,71 78,24] -> 80,95
 
-Console.WriteLine("Введите размер массива");
+Console.WriteLine("Enter array size");
 int size = Convert.ToInt32(Console.ReadLine());
 double[] numbers = new double[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
+Console.WriteLine("Here is our array: ");
 PrintArray(numbers);
 double min = Int32.MaxValue;
 double max = Int32.MinValue;
@@ -90,10 +97,8 @@ for (int z = 0; z < numbers.Length; z++)
             min = numbers[z];
         }
 }
-
-Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
-Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
-
+Console.WriteLine($"Total are {numbers.Length} of numbers. Maximal value = {max}, minimal value = {min}");
+Console.WriteLine($"Difference between maximal and minimal values = {max - min}");
 void FillArrayRandomNumbers(double[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
